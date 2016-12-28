d05b68e00199bc4e5af71b5313655fc65460cd10
Ebou Jobe <Ebou@dhcp212238.rez.mcgill.ca>
2016-12-28 04:52:32 -0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ebou Jobe on 2016-12-28.
+//  Copyright © 2016 Ebou Jobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
